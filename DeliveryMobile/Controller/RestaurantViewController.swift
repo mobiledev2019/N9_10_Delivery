1dbf78388cbfd8e187743a4e505a9c8ff268d109
cuongphamemail@gmail.com <cuongphamemail@gmail.com>
2019-04-02 22:56:38 +0700

    Fix Restaurant View List- Cuong 2/4

--- DeliveryMobile/Controller/RestaurantViewController.swift
+++ DeliveryMobile/Controller/RestaurantViewController.swift
@@ -14,6 +14,7 @@ class RestaurantViewController: UIViewController {
     
     var restaurants = [Restaurant]()
     var filteredRestaurants = [Restaurant]()
+    @IBOutlet weak var tbvRestaurant: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +24,11 @@ class RestaurantViewController: UIViewController {
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        loadRestaurants()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        
+    func loadRestaurants(){
         APIManager.shared.getRestaurants { (json) in
             
             if json != nil {
@@ -38,9 +40,23 @@ class RestaurantViewController: UIViewController {
                         let restaurant = Restaurant(json: item)
                         self.restaurants.append(restaurant)
                     }
+                    
+                    self.tbvRestaurant.reloadData()
                 }
             }
         }
+    }
+    func loadImage(imageView: UIImageView, urlString: String){
+        let imgURL : URL = URL(string: urlString)!
+        
+        URLSession.shared.dataTask(with: imgURL) { (data, response, error) in
+            
+            guard let data = data, error == nil else { return}
+            
+            DispatchQueue.main.async(execute: {
+                imageView.image = UIImage(data: data)
+            })
+        }.resume()
     }
 }
 
@@ -51,12 +67,22 @@ extension RestaurantViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.restaurants.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath) as! RestanrantViewCell
         
+        let restaurant: Restaurant
+        restaurant = restaurants[indexPath.row]
+        
+        cell.lbRestaurantName.text = restaurant.name!
+        cell.lbRestaurantAddress.text = restaurant.address!
+        
+        if let logoName = restaurant.logo {
+            let url = "\(logoName)"
+            loadImage(imageView: cell.imgRestaurantLogo, urlString: url)
+        }
         return cell
     }
     
---
+++ DeliveryMobile/View/RestanrantViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  RestanrantViewCell.swift
+//  DeliveryMobile
+//
+//  Created by Cuong Pham on 4/2/19.
+//  Copyright © 2019 Cuong Pham. All rights reserved.
+//
+
+import UIKit
+
+class RestanrantViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbRestaurantName: UILabel!
+    @IBOutlet weak var lbRestaurantAddress: UILabel!
+    @IBOutlet weak var imgRestaurantLogo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+}
