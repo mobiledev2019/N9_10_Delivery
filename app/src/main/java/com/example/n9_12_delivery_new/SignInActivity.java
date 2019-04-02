ff6e47344402b1521d09be1b5d3018188bab0902
NguyenKhacDuy <30316081+NguyenKhacDuy@users.noreply.github.com>
2019-04-02 14:32:52 +0700

    update 2/4/2019-Res_List-Tray-Fragment

--- app/src/main/java/com/example/n9_12_delivery_new/SignInActivity.java
+++ app/src/main/java/com/example/n9_12_delivery_new/SignInActivity.java
@@ -1,7 +1,10 @@
 package com.example.n9_12_delivery_new;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button signInFacebook = findViewById(R.id.loginFacebook);
+
+        signInFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    
 }
---
+++ app/src/main/java/com/example/n9_12_delivery_new/TrayFragment.java
@@ -0,0 +1,61 @@
+package com.example.n9_12_delivery_new;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TrayFragment extends Fragment {
+
+
+    public TrayFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tray, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ListView listView = getActivity().findViewById(R.id.trayList);
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return LayoutInflater.from(getActivity()).inflate(R.layout.list_item_tray,null);
+            }
+        });
+    }
+}
