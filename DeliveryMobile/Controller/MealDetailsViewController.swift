0af36953eb520f25c906efe207d4a8ad7f7e2858
cuongphamemail@gmail.com <cuongphamemail@gmail.com>
2019-04-03 15:13:33 +0700

    Add funtion to Button - Cuong 3/4

--- DeliveryMobile/Controller/MealDetailsViewController.swift
+++ DeliveryMobile/Controller/MealDetailsViewController.swift
@@ -14,7 +14,48 @@ class MealDetailsViewController: UIViewController {
     @IBOutlet weak var lbMealName: UILabel!
     @IBOutlet weak var lbMealShortDes: UILabel!
     
+    @IBOutlet weak var lbQty: UILabel!
+    @IBOutlet weak var lbTotal: UILabel!
+    
+    @IBAction func AddQty(_ sender: Any) {
+        
+        if qty < 99 {
+            qty+=1
+            lbQty.text = String(qty)
+            
+            if let price = meal?.price {
+                lbTotal.text = "$\(price * Float(qty))"
+            }
+        }
+    }
+    
+    @IBAction func removeQty(_ sender: Any) {
+        
+        if qty >= 2 {
+            qty-=1
+            lbQty.text = String(qty)
+            
+            if let price = meal?.price {
+                lbTotal.text = "$\(price * Float(qty))"
+            }
+        }
+    }
+    
+    @IBAction func addToTray(_ sender: Any) {
+        
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 40))
+        image.image = UIImage(named: "button_chicken")
+        image.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height-100)
+        self.view.addSubview(image)
+        
+        UIView.animate(withDuration: 1.0,
+                       delay: 0.0,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: { image.center = CGPoint(x: self.view.frame.width-40, y: 24) },
+                       completion: { _ in image.removeFromSuperview() })
+    }
     var meal: Meal?
+    var qty = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +63,10 @@ class MealDetailsViewController: UIViewController {
     }
     
     func loadMeal(){
+        
+        if let price = meal?.price {
+            lbTotal.text = "$\(price)"
+        }
         
         lbMealName.text = meal?.name
         lbMealShortDes.text = meal?.short_description
