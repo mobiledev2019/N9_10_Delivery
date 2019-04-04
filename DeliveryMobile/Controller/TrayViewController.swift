01b50c6cf4f1df2138e1005888c740c349995735
cuongphamemail@gmail.com <cuongphamemail@gmail.com>
2019-04-04 15:29:51 +0700

    Display Tray View - Cuong 4/4

--- DeliveryMobile/Controller/TrayViewController.swift
+++ DeliveryMobile/Controller/TrayViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
-
+import MapKit
 class TrayViewController: UIViewController {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    @IBOutlet weak var tbvMeals: UITableView!
+    @IBOutlet weak var viewTotal: UIView!
+    @IBOutlet weak var viewAddress: UIView!
+    @IBOutlet weak var viewMap: UIView!
+    @IBOutlet weak var lbTotal: UILabel!
+    @IBOutlet weak var tbAddress: UITextField!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var bAddPayment: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +28,33 @@ class TrayViewController: UIViewController {
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        if Tray.currentTray.items.count == 0 {
+            // Show message
+            
+            let lbEmptyTray = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 40))
+            lbEmptyTray.center = self.view.center
+            lbEmptyTray.textAlignment = NSTextAlignment.center
+            lbEmptyTray.text = "Your Tray is empty. Please select meal!"
+            
+            self.view.addSubview(lbEmptyTray)
+        } else {
+            // Display UI controller
+            
+            self.tbvMeals.isHidden = false
+            self.viewTotal.isHidden = false
+            self.viewAddress.isHidden = false
+            self.bAddPayment.isHidden = false
+            self.viewMap.isHidden = false
+            
+            loadMeals()
+        }
+    }
+    
+    func loadMeals() {
+        
+        self.tbvMeals.reloadData()
+        self.lbTotal.text = "$\(Tray.currentTray.getTotal())"
     }
 }
 
