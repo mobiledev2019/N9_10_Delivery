6f7a2ab12da748669ec56b722ba698d2effa7874
NguyenKhacDuy <30316081+NguyenKhacDuy@users.noreply.github.com>
2019-04-09 07:55:31 +0700

    update 4/9/2019

--- app/src/main/java/com/example/n9_12_delivery_new/SignInActivity.java
+++ app/src/main/java/com/example/n9_12_delivery_new/SignInActivity.java
@@ -1,28 +1,94 @@
 package com.example.n9_12_delivery_new;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignInActivity extends AppCompatActivity {
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sign_in);
 
-        Button signInFacebook = findViewById(R.id.loginFacebook);
+        LoginButton signInFacebook = findViewById(R.id.loginFacebook);
+        final Button customer = findViewById(R.id.customerLogin);
+        final Button driver = findViewById(R.id.driverLogin);
 
-        signInFacebook.setOnClickListener(new View.OnClickListener() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        final boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+        customer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!isLoggedIn) {
+                    Toast.makeText(getApplicationContext(),"Login First", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isLoggedIn) {
+                    Toast.makeText(getApplicationContext(),"Login First", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DriverMainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        signInFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+
                 Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
                 startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
             }
         });
     }
 
-    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
