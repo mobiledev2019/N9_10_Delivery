a9fcf53649c63308a4da6c1014c57f2d183aa3f4
cuongphamemail@gmail.com <cuongphamemail@gmail.com>
2019-04-12 13:00:10 +0700

    Complete OrderViewController - Cuong 12/4

--- DeliveryMobile/Controller/OrderViewController.swift
+++ DeliveryMobile/Controller/OrderViewController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import SwiftyJSON
+import MapKit
 
 class OrderViewController: UIViewController {
 
+    @IBOutlet weak var tbvMeal: UITableView!
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var lbStatus: UILabel!
+    
+    var tray = [JSON]()
+    
+    var destination: MKPlacemark?
+    var source: MKPlacemark?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +31,77 @@ class OrderViewController: UIViewController {
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        getLatestOrder()
+    }
+    
+    func getLatestOrder(){
+
+        APIManager.shared.getLatestOrder { (json) in
+            
+            print(json)
+            
+            let order = json["order"]
+            
+            if let orderDetails = order["order_details"].array {
+
+                self.lbStatus.text = order["status"].string!.uppercased()
+                self.tray = orderDetails
+                self.tbvMeal.reloadData()
+            }
+            
+            let from = order["restaurant"]["address"].string!
+            let to = order["address"].string!
+            
+            self.getLocation(from, "Restaurant", { (sou) in
+                self.source = sou
+                
+                self.getLocation(to, "Customer", { (des) in
+                    self.destination = des
+                })
+            })
+        }
+    }
+}
+
+extension OrderViewController: MKMapViewDelegate {
+    
+    // #1 - Delegate method of MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 5.0
+        
+        return renderer
+    }
+    
+    // #2 - convert an address string to a location on the map
+    func getLocation (_ address: String,_ title: String, _ completionHandler: @escaping (MKPlacemark) -> Void) {
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            
+            if (error != nil) {
+                print("Error: ", error!)
+            }
+            
+            if let placemark = placemarks?.first {
+                
+                let coordinates: CLLocationCoordinate2D = placemark.location!.coordinate
+                
+                // Create Pin
+                let dropPin = MKPointAnnotation()
+                dropPin.coordinate = coordinates
+                dropPin.title = title
+                
+                self.map.addAnnotation(dropPin)
+                completionHandler(MKPlacemark.init(placemark: placemark))
+            }
+        }
+        
     }
 }
 
@@ -28,11 +110,16 @@ extension OrderViewController: UITableViewDataSource, UITableViewDelegate {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return tray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderItemCell", for: indexPath) as! OrderViewCell
+        
+        let item = tray[indexPath.row]
+        cell.lbQty.text = String(item["quantity"].int!)
+        cell.lbMealName.text = item["meal"]["name"].string
+        cell.lbSubTotal.text = "$\(String(item["sub_total"].float!))"
         
         return cell
     }
---
+++ DeliveryMobile/View/OrderViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  OrderViewCell.swift
+//  DeliveryMobile
+//
+//  Created by Cuong Pham on 4/12/19.
+//  Copyright © 2019 Cuong Pham. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbQty: UILabel!
+    @IBOutlet weak var lbMealName: UILabel!
+    @IBOutlet weak var lbSubTotal: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
